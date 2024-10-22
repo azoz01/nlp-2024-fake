203bf19737788402fbb2f606d2a083651b8c30c1
Antoni Zajko <antoni.zajko.1@gmail.com>
2024-10-22 20:49:00 +0200

    Add slightly better model

--- poc.py
+++ poc.py
@@ -1,9 +1,7 @@
-from typing import Mapping
+import datetime
 
 import evaluate
 import numpy as np
-import pandas as pd
-from datasets import Dataset
 from loguru import logger
 from transformers import (
     AutoConfig,
@@ -13,50 +11,9 @@ from transformers import (
     TrainingArguments,
 )
 
-COLUMNS = [
-    "statement_id",
-    "label",
-    "statement",
-    "subject",
-    "speaker",
-    "speaker_job",
-    "state",
-    "party",
-    "true_cnt",
-    "false_cnt",
-    "half_true_cnt",
-    "mostly_true_cnt",
-    "pants_on_fire_cnt",
-    "context",
-]
-LABEL_MAPPING = {
-    "false": 1,
-    "pants-fire": 1,
-    "mostly-true": 0,
-    "true": 0,
-}
-STATEMENT_COLUMN = "statement"
-LABEL_COLUMN = "label"
+from engine.data import prepare_data_for_fine_tuning, read_data
+
 MODEL_ID = "roberta-base"
-
-
-def read_data(
-    path: str,
-    columns: list[str] = COLUMNS,
-    label_mapping: Mapping[str, int] = LABEL_MAPPING,
-) -> pd.DataFrame:
-    df = pd.read_table(path, header=None, names=columns)
-    df = df[[STATEMENT_COLUMN, LABEL_COLUMN]]
-    df[LABEL_COLUMN] = df[LABEL_COLUMN].map(label_mapping)
-    df = df.loc[~df[LABEL_COLUMN].isnull()]
-    df[LABEL_COLUMN] = df[LABEL_COLUMN].astype(int)
-    assert (df.isna().sum() == 0).all()
-    assert (
-        df[LABEL_COLUMN].drop_duplicates().sort_values().values == [0, 1]
-    ).all()
-    df = df.rename(columns={STATEMENT_COLUMN: "text"})
-    return df
-
 
 logger.info("Loading data")
 train = read_data("data/split_raw/train.tsv")
@@ -66,33 +23,14 @@ test = read_data("data/split_raw/test.tsv")
 logger.info("Loading tokenizer")
 tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
 
-
-def prepare_data_for_fine_tuning(df: pd.DataFrame):
-    dataset = Dataset.from_pandas(df)
-
-    def tokenize(batch):
-        return tokenizer(
-            batch["text"],
-            padding="max_length",
-            max_length=256,
-            truncation=True,
-        )
-
-    dataset = dataset.map(tokenize)
-    dataset.set_format(
-        "torch", columns=["input_ids", "attention_mask", "label"]
-    )
-    return dataset
-
-
 logger.info("Preparing data for fine-tuning")
-train_dataset = prepare_data_for_fine_tuning(train)
-valid_dataset = prepare_data_for_fine_tuning(valid)
-test_dataset = prepare_data_for_fine_tuning(test)
+train_dataset = prepare_data_for_fine_tuning(train, tokenizer)
+valid_dataset = prepare_data_for_fine_tuning(valid, tokenizer)
+test_dataset = prepare_data_for_fine_tuning(test, tokenizer)
 
 
 logger.info("Preparing model for fine-tuning")
-metric = evaluate.load("f1")
+metric = evaluate.load("accuracy")
 
 
 def compute_metrics(eval_pred):
@@ -105,11 +43,12 @@ config = AutoConfig.from_pretrained(MODEL_ID)
 model = AutoModelForSequenceClassification.from_pretrained(
     MODEL_ID, config=config
 )
+timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 training_args = TrainingArguments(
-    output_dir="output",
-    num_train_epochs=5,
+    output_dir=f"output/{timestamp}",
+    num_train_epochs=15,
     per_device_train_batch_size=16,
-    per_device_eval_batch_size=64,
+    per_device_eval_batch_size=128,
     eval_strategy="epoch",
     logging_strategy="steps",
     logging_steps=10,
@@ -120,6 +59,9 @@ training_args = TrainingArguments(
     load_best_model_at_end=True,
     save_total_limit=2,
     report_to="tensorboard",
+    seed=123,
+    greater_is_better=True,
+    metric_for_best_model="eval_accuracy",
 )
 trainer = Trainer(
     model=model,
@@ -130,3 +72,6 @@ trainer = Trainer(
 )
 logger.info("Training model")
 trainer.train()
+trainer.save_model(
+    f"output/{timestamp}/model",
+)
