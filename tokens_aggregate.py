e5a212b83c81efb44267ebb70ff3169a83a3f674
Piotr Robak <piotr.robak.stud@pw.edu.pl>
2024-11-20 18:13:56 +0100

    some refactor

--- tokens_aggregate.py
+++ tokens_aggregate.py
@@ -1,86 +1,86 @@
+from typing import Self
 import spacy
-class token_aggregate:
+from torch import Tensor
+
+class TokenAggregate:
     def __init__(
         self,
-        spacy_token,
-        clean_model_tokens,
-        dirty_model_tokens,
-        is_spacy_NER,
-        model_exp
-    ):
+        spacy_token: str,
+        clean_model_tokens: list[str],
+        dirty_model_tokens: list[str],
+        is_spacy_NER: bool,
+        model_exp: list[float]
+    ) ->None:
         self.spacy_token = spacy_token
         self.clean_model_tokens = clean_model_tokens
         self.dirty_model_tokens = dirty_model_tokens
         self.is_spacy_NER = is_spacy_NER
         self.model_exp = model_exp
-
-    def get_spacy_token(self):
-        return self.spacy_token
-    def get_model_tokens(self):
-        return self.clean_model_tokens, self.dirty_model_tokens
-    def get_model_exp(self):
-        return self.model_exp
-
-def generate_aggregate_list(doc,exp_tensor,tokens_clear,tokens_dirty):
-    spacy_token_to_our_tokens = []
-
-    spacy_tokens = []
-    spacy_is_NER = []
-
-    current_clear_token = 0
-    current_spacy_token = 0
-
-
-    constructed_token = ""
-    clean_tokens_for_current_spacy_token = []
-    dirty_tokens_for_current_spacy_token = []
-    model_token_exp = []
-
-    for token in doc:
-        spacy_tokens.append(token.text)
-        spacy_is_NER.append(token.ent_type_ != '')
-
-    for token in tokens_clear:
-
-        constructed_token = constructed_token+token
         
-        clean_tokens_for_current_spacy_token.append(token)
-        #weird shift
-        dirty_tokens_for_current_spacy_token.append(tokens_dirty[current_clear_token+1])
-        model_token_exp.append(exp_tensor[0,0,current_clear_token+1])
+    @staticmethod
+    def generate_aggregate_list(
+        doc: spacy.tokens.doc.Doc,
+        exp_tensor: Tensor,
+        tokens_clear: list[str],
+        tokens_dirty: list[str]):
 
-        if(constructed_token == spacy_tokens[current_spacy_token]):
+        spacy_token_to_our_tokens = []
 
-            new_aggregate = token_aggregate(
-                spacy_tokens[current_spacy_token],
-                clean_tokens_for_current_spacy_token,
-                dirty_tokens_for_current_spacy_token,
-                spacy_is_NER[current_spacy_token],
-                model_token_exp
-                )
-            spacy_token_to_our_tokens.append(new_aggregate)
+        spacy_tokens = []
+        spacy_is_NER = []
 
-            current_spacy_token= current_spacy_token+1
-
-            constructed_token = ""
-            clean_tokens_for_current_spacy_token = []
-            dirty_tokens_for_current_spacy_token = []
-            model_token_exp = []
-        current_clear_token = current_clear_token+1
+        current_clear_token = 0
+        current_spacy_token = 0
 
 
-    #debug!!!
-    if(current_spacy_token != len(spacy_tokens)):
-        print(f"\n INVALID DOC!!! stopped at {spacy_tokens[current_spacy_token]}\n")
-        print(f"{constructed_token}")
-        print(f"spacy tokens left: {spacy_tokens[current_spacy_token:]}")
-        print(f"model tokens left: {tokens_clear[sum([len(x.get_model_tokens()[0]) for x in spacy_token_to_our_tokens]):]}")
-        for token in doc:
-            if token.ent_type_:
-                print(f"Token: {token.text} True")
-            else:
-                print(f"Token: {token.text} False")
+        constructed_token = ""
+        clean_tokens_for_current_spacy_token = []
+        dirty_tokens_for_current_spacy_token = []
+        model_token_exp = []
 
+        for spacy_token in doc:
+            spacy_tokens.append(spacy_token.text)
+            spacy_is_NER.append(spacy_token.ent_type_ != '')
 
-    
-    return spacy_token_to_our_tokens
+        for token in tokens_clear:
+
+            constructed_token = constructed_token+token
+            
+            clean_tokens_for_current_spacy_token.append(token)
+            #weird shift
+            dirty_tokens_for_current_spacy_token.append(tokens_dirty[current_clear_token+1])
+            model_token_exp.append(float(exp_tensor[0,0,current_clear_token+1]))
+
+            if(constructed_token == spacy_tokens[current_spacy_token]):
+
+                new_aggregate = TokenAggregate(
+                    spacy_tokens[current_spacy_token],
+                    clean_tokens_for_current_spacy_token,
+                    dirty_tokens_for_current_spacy_token,
+                    spacy_is_NER[current_spacy_token],
+                    model_token_exp
+                    )
+                spacy_token_to_our_tokens.append(new_aggregate)
+
+                current_spacy_token= current_spacy_token+1
+
+                constructed_token = ""
+                clean_tokens_for_current_spacy_token = []
+                dirty_tokens_for_current_spacy_token = []
+                model_token_exp = []
+            current_clear_token = current_clear_token+1
+            
+        #debug!!!
+        if(current_spacy_token != len(spacy_tokens)):
+            print(f"\n INVALID DOC!!! stopped at {spacy_tokens[current_spacy_token]}\n")
+            print(f"{constructed_token}")
+            print(f"spacy tokens left: {spacy_tokens[current_spacy_token:]}")
+            print(f"model tokens left: {tokens_clear[sum([len(x.clean_model_tokens) for x in spacy_token_to_our_tokens]):]}")
+
+            for spacy_token in doc:
+                if spacy_token.ent_type_:
+                    print(f"Token: {spacy_token.text} True")
+                else:
+                    print(f"Token: {spacy_token.text} False")
+
+        return spacy_token_to_our_tokens
