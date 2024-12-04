d805f52dbbba599b32251b9b7b99c5616f23af08
DawidPludowski <dawid.pludowski@gmail.com>
2024-12-04 22:31:03 +0100

    add poc of counterfactual; format code

--- engine/tokens_aggregate.py
+++ engine/tokens_aggregate.py
@@ -1,6 +1,6 @@
-from typing import Self
 import spacy
 from torch import Tensor
+
 
 class TokenAggregate:
     def __init__(
@@ -9,8 +9,8 @@ class TokenAggregate:
         clean_model_tokens: list[str],
         dirty_model_tokens: list[str],
         NERs: list[str],
-        model_exp: list[float]
-    ) ->None:
+        model_exp: list[float],
+    ) -> None:
         self.spacy_token = spacy_token
         self.clean_model_tokens = clean_model_tokens
         self.dirty_model_tokens = dirty_model_tokens
@@ -25,13 +25,14 @@ class TokenAggregate:
             f"Our model dirty: {self.dirty_model_tokens}\n"
             f"model exp: {self.model_exp}\n"
         )
-        
+
     @staticmethod
     def generate_aggregate_list(
         doc: spacy.tokens.doc.Doc,
         exp_tensor: Tensor,
         tokens_clear: list[str],
-        tokens_dirty: list[str]):
+        tokens_dirty: list[str],
+    ):
 
         spacy_token_to_our_tokens = []
 
@@ -40,7 +41,6 @@ class TokenAggregate:
 
         current_clear_token_id = 0
         current_spacy_token_id = 0
-
 
         constructed_model_token = ""
         constructed_spacy_token = ""
@@ -54,38 +54,50 @@ class TokenAggregate:
             spacy_tokens.append(spacy_token.text)
             NER.append(spacy_token.ent_type_)
 
-        #xdd
-        if(spacy_tokens[0]==" "):
+        # xdd
+        if spacy_tokens[0] == " ":
             current_spacy_token_id = 1
 
-        while current_clear_token_id<len(tokens_clear) or current_spacy_token_id<len(spacy_tokens):
-            if(current_clear_token_id>len(tokens_clear)) or current_spacy_token_id>len(spacy_tokens):
+        while current_clear_token_id < len(
+            tokens_clear
+        ) or current_spacy_token_id < len(spacy_tokens):
+            if (
+                current_clear_token_id > len(tokens_clear)
+            ) or current_spacy_token_id > len(spacy_tokens):
                 break
 
-            if( len(constructed_model_token)< len(constructed_spacy_token)):
+            if len(constructed_model_token) < len(constructed_spacy_token):
 
                 constructed_model_token += tokens_clear[current_clear_token_id]
-                clean_tokens_for_current_spacy_tokens.append(tokens_clear[current_clear_token_id])
-                dirty_tokens_for_current_spacy_tokens.append(tokens_dirty[current_clear_token_id+1])
-                model_token_exp.append(float(exp_tensor[0,current_clear_token_id+1]))
-                current_clear_token_id +=1
+                clean_tokens_for_current_spacy_tokens.append(
+                    tokens_clear[current_clear_token_id]
+                )
+                dirty_tokens_for_current_spacy_tokens.append(
+                    tokens_dirty[current_clear_token_id + 1]
+                )
+                model_token_exp.append(
+                    float(exp_tensor[0, current_clear_token_id + 1])
+                )
+                current_clear_token_id += 1
             else:
-                if(current_spacy_token_id == len(spacy_tokens)):
+                if current_spacy_token_id == len(spacy_tokens):
                     break
                 constructed_spacy_token += spacy_tokens[current_spacy_token_id]
                 constructed_NERs.append(NER[current_spacy_token_id])
-                spacy_token_to_current_model_tokens.append(spacy_tokens[current_spacy_token_id])
-                current_spacy_token_id +=1
-                
-            if(constructed_model_token == constructed_spacy_token):
+                spacy_token_to_current_model_tokens.append(
+                    spacy_tokens[current_spacy_token_id]
+                )
+                current_spacy_token_id += 1
+
+            if constructed_model_token == constructed_spacy_token:
 
                 new_aggregate = TokenAggregate(
                     spacy_token_to_current_model_tokens,
                     clean_tokens_for_current_spacy_tokens,
                     dirty_tokens_for_current_spacy_tokens,
                     constructed_NERs,
-                    model_token_exp
-                    )
+                    model_token_exp,
+                )
                 spacy_token_to_our_tokens.append(new_aggregate)
 
                 constructed_model_token = ""
@@ -96,14 +108,11 @@ class TokenAggregate:
                 dirty_tokens_for_current_spacy_tokens = []
                 model_token_exp = []
 
-
-            
-        #debug!!!
-        if(len(constructed_model_token)):
+        # debug!!!
+        if len(constructed_model_token):
             print(f"\n INVALID DOC!!! stopped")
             print(constructed_spacy_token)
             print(constructed_model_token)
             return False
-
 
         return spacy_token_to_our_tokens
